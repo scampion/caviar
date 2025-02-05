1a715ffd9564843fd382ef2a7ef4792e30017522
Sébastien Campion <sebastien.campion@gmail.com>
2025-02-05 21:57:56 +0100

    fix: Cast page_id.1 to u16 in replace_page_text for change_page_content

--- src/main.rs
+++ src/main.rs
@@ -117,11 +117,11 @@ fn extract_text_from_content(content: &Vec<u8>) -> String {
 
 fn replace_page_text(doc: &mut Document, page_id: (u32, u32), text: &str) -> Result<(), lopdf::Error> {
     // Create new content with sanitized text
-    let new_content = vec![Object::string_literal(text)];
-    
+    let new_content = vec![Object::string_literal(text.as_bytes())];
+
     // Replace page content
-    doc.change_page_content(page_id, new_content)?;
-    
+    doc.change_page_content((page_id.0, page_id.1 as u16), new_content)?;
+
     Ok(())
 }
 
